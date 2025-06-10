e632b4dce9d49b2e7da5a91e69e531ab6cdffdcf
Sai77444 <you@example.com>
2025-06-10 16:33:36 +0530

    Day 1

---
+++ java/Day1/Hello.java
@@ -0,0 +1,9 @@
+package Demo.java;
+
+public class Hello {
+
+	public static void main(String[] args) {
+	System.out.print("Hello Sai);
+	}
+
+}
---
+++ java/Day1/Hello2.java
@@ -0,0 +1,12 @@
+package Demo.java;
+
+public class Hello2 {
+
+	public static void main(String[] args) {
+		System.out.println("My name is Sai");
+		System.out.println("Course is Full Stack JAva");
+		System.out.println("K.K.Wagh Polytechnic");
+
+	}
+
+}
